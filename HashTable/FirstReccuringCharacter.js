79a1f57c46f0ec53f557ff26cf57635fe878f3a6
samarthg95 <samarthmg@gmail.com>
2020-06-19 19:52:24 +0530

    added comments

--- HashTable/FirstReccuringCharacter.js
+++ HashTable/FirstReccuringCharacter.js
@@ -1,9 +1,18 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
 function firstReccuringCharacter(inputArray){
   for (let i = 1; i < input.length; i++) {
     for (let j = i - 1; j >= 0; j--) {
       
       if (input[i] === input[j]) {
-        console.log('i = '+ i + ' j ='+j)
         return input[i];
       }
     }
